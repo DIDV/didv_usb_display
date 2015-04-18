248f72420fb79bdeac41da25b11e9288dcad7f8a
Vitor Hugo <toorugoo@gmail.com>
2015-04-18 16:15:23 -0300

    Código modificado para ser classe

--- usb.rb
+++ usb.rb
@@ -4,52 +4,56 @@
 
 require "serialport"
 require "pry"
-#require "eventmachine"
 
-# params for serial port
-# usaly in the form of /dev/tty.usbmodem*
-#port_str = "/dev/ttyACM0"
-port_str = "/dev/ttyACM1"  #may be different for you
-baud_rate = 57600
-data_bits = 8
-stop_bits = 1
-parity = SerialPort::NONE
+module DIDV
+  class Serial_communication
 
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
-a = Array.new #test array
-(0..10).each {|data| a[data] = (data).chr}
+    attr_accessor :sp
+    def initialize(baud_rate=57600, data_bits=8, stop_bits=1, parity=SerialPort::NONE)
+      port_str = "/dev/ttyACM1"  #may be different for you
+      @sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+    end
+
+    def packetize_data data
+      data_to_send = Array.new
+      data_to_send[0] = 0x62.chr
+      data_to_send[11] = 0x65.chr
+      (1..10).each { |i| data_to_send[i] = data[i-1] }
+      send_and_get_data data_to_send
+    end
+
+    def send_data data
+      puts "Sending '#{data}'..."
+      data.each { |c| sp.putc c }
+      puts "Data sent :'#{data}'"
+    end
+
+    def get_data times=nil
+      # this first version just gets data forever
+      puts "Getting getting data..."
+      if (times)
+        times.times { puts sp.getc }
+      else
+        #TODO pensar na recepção
+        loop { foward_received_data(sp.gets) }
+      end
+    end
+
+    def send_and_get_data data
+      send_data data
+      get_data data.size
+    end
+
+    def foward_received_data data
+      puts "#{data}" #modify to send data to somewhere that it makes sense
+    end
+
+  end
+  serial = Serial_communication.new
+  a = Array.new #test array
+  (0..10).each {|data| a[data] = (data).chr}
+  serial.packetize_data a
+  binding.pry
 
 
-def packetize_data sp, data
-  data_to_send = Array.new
-  data_to_send[0] = 0x62.chr
-  data_to_send[11] = 0x65.chr
-  (1..10).each { |i| data_to_send[i] = data[i-1] }
-  send_and_get_data sp, data_to_send
 end
-
-def send_data sp, data
-  puts "Sending '#{data}'..."
-  data.each { |c| sp.putc c }
-  puts "Data sent :'#{data}'"
-end
-
-def get_data sp#, times=nil
-  # this first version just gets data forever
-  puts "Getting getting data..."
- #"{ if (times)
-  #  times.times { puts sp.getc }
-  #else}"
-    loop { puts sp.getc }
-  #end
-end
-
-def send_and_get_data sp, data
-  send_data sp, data
-  get_data sp#, data.size
-end
-
-
-binding.pry
-
-sp.close
