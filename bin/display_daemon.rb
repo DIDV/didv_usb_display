8aab17b9153c7d06709a14df5d33f75f10ca379b
Lucas Vieira <lucas@vieira.io>
2015-05-14 01:23:37 -0300

    daemon response

--- bin/display_daemon.rb
+++ bin/display_daemon.rb
@@ -8,10 +8,12 @@ module DIDV
 
     def post_init
       @display = Display.new
+      send_data 'waiting'
     end
 
     def receive_data(input)
       @display.send_hex input if @display.valid? input
+      send_data 'waiting'
     end
 
   end
